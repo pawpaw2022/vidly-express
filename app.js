8e5ad752f3fc71e9331fa7f85c69e43a486ffb36
paul <paul.l.sining@gmail.com>
2022-12-12 23:43:52 -0800

    adding restful api

---
+++ app.js
@@ -0,0 +1,85 @@
+// Build a web server
+const express = require("express");
+const Joi = require("joi");
+const app = express();
+
+// add middleware 
+app.use(express.json());
+
+// init genres 
+const genres = [
+    { id: 1, name: "Comedy" }, 
+    { id: 2, name: "Honor" }, 
+    { id: 3, name: "Documentary"}
+]
+
+// Get all the genres 
+app.get("/api/genres", (req, res) =>{
+    res.send(genres);
+})
+
+// add a new genre
+app.post("/api/genres", (req, res)=>{
+    // Create the genre and return the genre object  
+	const genre = {
+		id: genres.length+1, 
+		name: req.body.name
+	}; 
+
+  // validation 
+	const { error } = validateGenres(genre);
+	if(error) {
+		res.status(400).send(error.details[0].message);
+		return;
+	}
+
+	genres.push(genre);
+	
+	res.send(genre); 
+})
+
+
+// Updating a genre
+app.put("/api/genres/:id", (req, res) => {      
+	// If genre not found, return 404, otherwise update it 
+	let genre = genres.find(c => c.id === parseInt(req.params.id));
+	if (!genre) res.status(404).send("Genre not found");
+
+	// validation 
+	const { error } = validateGenres(req.body);
+	if(error) {
+		res.status(400).send(error.details[0].message);
+		return;
+	}
+ 
+	// and return the updated object. 
+	genre.name = req.body.name;
+	res.send(genre); 
+});
+
+// Deleting a genre 
+app.delete("/api/genres/:id", (req, res) => {      
+	// If genre not found, return 404, otherwise delete it     
+	const genre = genres.find(c => c.id === parseInt(req.params.id));
+	if (!genre) res.status(404).send("genre not found!");
+
+	// delete 
+	const index = genres.indexOf(genre);
+	genres.splice(index, 1);	
+
+	// and return the deleted object. 
+	res.send(genre);
+});
+
+// validation
+function validateGenres(genre) {
+	const schema = Joi.object({
+		name: Joi.string().min(3).required()
+	});
+
+	return schema.validate(genre, { allowUnknown:true });
+}
+
+// Listen on port 3000
+const port = process.env.PORT || 3000; // export PORT=4000 
+app.listen(port, ()=>console.log(`Listening to port ${port}`));
