f709c79ca0f1bfd43979a607478c87d79ca25de4
paul <paul.l.sining@gmail.com>
2023-01-04 00:12:44 -0800

    refactor return api

--- models/rental.js
+++ models/rental.js
@@ -2,63 +2,79 @@
 
 const Joi = require("joi");
 const mongoose = require("mongoose");
+const moment = require("moment/moment");
 
-const Rental = mongoose.model(
-  "Rental",
-  new mongoose.Schema({
-    // we don't import schema as we only need certain props
-    customer: {
-      type: new mongoose.Schema({
-        name: {
-          type: String,
-          required: true,
-          minlength: 1,
-          maxlength: 20,
-        },
-        isGold: {
-          type: Boolean,
-          default: false,
-        },
-        phone: {
-          type: String,
-          required: true,
-          minlength: 5,
-          maxlength: 20,
-        },
-      }),
-      required: true,
-    },
-    movie: {
-      type: new mongoose.Schema({
-        title: {
-          type: String,
-          required: true,
-          trim: true,
-          minlength: 3,
-          maxlength: 255,
-        },
-        dailyRentalRate: {
-          type: Number,
-          required: true,
-          default: 4.99,
-        },
-      }),
-      required: true,
-    },
-    dateOut: {
-      type: Date,
-    },
-    dateReturned: {
-      type: Date,
-    },
-    rentalFee: {
-      type: Number,
-      min: 0,
-      default: 0,
-    },
-  })
-);
+const rentalSchema = new mongoose.Schema({
+  // we don't import schema as we only need certain props
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 20,
+      },
+      isGold: {
+        type: Boolean,
+        default: false,
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 20,
+      },
+    }),
+    required: true,
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 255,
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        default: 4.99,
+      },
+    }),
+    required: true,
+  },
+  dateOut: {
+    type: Date,
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+});
 
+// define static class method
+rentalSchema.statics.lookup = function (customerId, movieId) {
+  return this.findOne({
+    "customer._id": customerId,
+    "movie._id": movieId,
+  });
+};
+
+rentalSchema.methods.proceedReturn = function () {
+  // Set the return date
+  this.dateReturned = moment().toDate();
+  
+  // Calculate the rental fee
+  const diff = moment().diff(this.dateOut, "days");
+  this.rentalFee = this.movie.dailyRentalRate * diff;
+};
+
+const Rental = mongoose.model("Rental", rentalSchema);
 // validation w Joi
 function validateRentals(movie) {
   const schema = Joi.object({
--- routers/returns.js
+++ routers/returns.js
@@ -1,38 +1,33 @@
 /** @format */
 
 const express = require("express");
-const { escapeRegExp } = require("lodash");
+const Joi = require("joi");
 const auth = require("../middleware/auth");
 const { Customer } = require("../models/customer");
 const { Movie } = require("../models/movie");
 const { Rental } = require("../models/rental");
 const route = express.Router();
 const mongoose = require("mongoose");
-const moment = require("moment/moment");
 
-//
-route.post("/", auth, async (req, res) => {
-  // no customerId
-  if (!req.body.customerId)
-    return res.status(400).send("No customerId provided");
+const validate = (validator) => {
+  return (req, res, next) => {
+    const { error } = validator(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    next();
+  };
+};
 
-  // no movieId
-  if (!req.body.movieId) return res.status(400).send("No movieId provided");
-
+route.post("/", [auth, validate(validateReturn)], async (req, res) => {
   const customer = await Customer.findById(req.body.customerId);
+  const movie = await Movie.findById(req.body.movieId);
   // customer doesn't exist in customer db
   if (!customer)
     return res.status(404).send("No customer is found with the given id");
-
-  const movie = await Movie.findById(req.body.movieId);
   // movie doesn't exist in movie db
   if (!movie)
     return res.status(404).send("No movie is found with the given id");
 
-  const rental = await Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId,
-  });
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
   if (!rental) return res.status(404).send("No rental is found");
 
   // rental already processed
@@ -41,13 +36,7 @@ route.post("/", auth, async (req, res) => {
 
   // valid request:
 
-  // Set the return date
-  rental.dateReturned = moment().toDate();
-  // Calculate the rental fee
-  const diff = moment().diff(rental.dateOut, "days");
-  rental.rentalFee = rental.movie.dailyRentalRate * diff;
-
-  console.log(diff);
+  rental.proceedReturn();
 
   // 2 phase commits
   try {
@@ -68,5 +57,15 @@ route.post("/", auth, async (req, res) => {
     return res.status(400).send("Failed to return the movie: ", err.message);
   }
 });
+
+// validation w Joi
+function validateReturn(obj) {
+  const schema = Joi.object({
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required(),
+  });
+
+  return schema.validate(obj, { allowUnknown: true });
+}
 
 module.exports = route;
